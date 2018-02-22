e6d98e4d877636f5aeccd3bb8bbabee4c60dda94
Christopher Fernandez <micronixon30@gmail.com>
2018-02-22 15:02:44 -0500

    updated News model, added django-ckeditor

--- jobs_first/jobs_first/settings.py
+++ jobs_first/jobs_first/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'about_us',
     'our_work',
     'publications',
-    'media'
+    'media',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
--- jobs_first/media/models.py
+++ jobs_first/media/models.py
@@ -2,17 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class News(models.Model):
-    external_link = models.URLField()
+    external_link = models.URLField(blank=True)
     publish_date = models.DateField()
-    image = models.
-    title = models.CharField(max_length=200, blank=True)
-    text = models.CharField(max_length=5000, blank=True) -- rich text field
+    image = models.ImageField(null=True)
+    publisher_name = models.CharField(max_length=200,blank=True)
+    title = models.CharField(max_length=200)
+    text = RichTextField(blank=True)
     author_first_name = models.CharField(max_length=200)
     author_last_name = models.CharField(max_length=200)
-    is_inPress = models.BooleanField(default=False)
-    is_blogPost = models.BooleanField(default=False)
+    slug = models.SlugField(blank=True)
+    publish = models.BooleanField(default=False)
+    is_news = models.BooleanField(default=False)
 
 
     class Meta:
